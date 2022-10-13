f86d1dfff1ccc54baf232ea7df6b2569168b558d
deshik91 <deshik91@ugar.xyz>
2022-10-13 20:49:40 +0300

    Add BuildBattle instance

--- BuildBattle/src/main/java/net/thisisnico/lolz/buildbattle/BuildBattle.java
+++ BuildBattle/src/main/java/net/thisisnico/lolz/buildbattle/BuildBattle.java
@@ -1,12 +1,18 @@
 package net.thisisnico.lolz.buildbattle;
 
+import lombok.Getter;
+import net.thisisnico.lolz.buildbattle.listeners.StreamHandler;
 import net.thisisnico.lolz.bukkit.BukkitUtils;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BuildBattle extends JavaPlugin {
 
+    @Getter
+    private static BuildBattle instance;
+
     @Override
     public void onEnable() {
+        instance = this;
 
         BukkitUtils.instantiate(this);
 
