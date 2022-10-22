767969b2dc80d83008f071cb8df80d03a1bfac50
deshik91 <deshik91@ugar.xyz>
2022-10-22 19:46:36 +0300

    Просто похуй на коммиты

--- BuildBattle/src/main/java/net/thisisnico/lolz/buildbattle/BuildBattle.java
+++ BuildBattle/src/main/java/net/thisisnico/lolz/buildbattle/BuildBattle.java
@@ -5,14 +5,21 @@ import net.thisisnico.lolz.buildbattle.listeners.StreamHandler;
 import net.thisisnico.lolz.bukkit.BukkitUtils;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public final class BuildBattle extends JavaPlugin {
 
     @Getter
     private static BuildBattle instance;
 
+    @Getter
+    private static Logger log;
+
     @Override
     public void onEnable() {
         instance = this;
+
+        log = getLogger();
 
         BukkitUtils.instantiate(this);
         BukkitUtils.registerListener(new StreamHandler());
--- BuildBattle/src/main/java/net/thisisnico/lolz/buildbattle/listeners/StreamHandler.java
+++ BuildBattle/src/main/java/net/thisisnico/lolz/buildbattle/listeners/StreamHandler.java
@@ -9,6 +9,7 @@ import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -18,14 +19,15 @@ public class StreamHandler implements Listener {
     @EventHandler
     void onJoin(PlayerJoinEvent e) {
         e.getPlayer().getInventory().clear();
-
-        if (Game.isStarted() && !Game.isPlayer(e.getPlayer())) {
+        e.getPlayer().setGameMode(GameMode.SURVIVAL);
+        if (Game.isStarted() &&
+                (!Game.isPlayer(e.getPlayer()) || DatabaseAdapter.getUser(e.getPlayer()).isAdmin())) {
             e.getPlayer().setGameMode(GameMode.SPECTATOR);
         }
 
-        if (DatabaseAdapter.getUser(e.getPlayer()).isAdmin()) {
-            e.getPlayer().getInventory().addItem(Game.getItem().getItemStack());
-        }
+//        if (DatabaseAdapter.getUser(e.getPlayer()).isAdmin()) {
+        e.getPlayer().getInventory().addItem(Game.getItem().getItemStack());
+//        }
     }
 
     @EventHandler
@@ -61,6 +63,30 @@ public class StreamHandler implements Listener {
 
     @EventHandler
     void onPlayerBreak(BlockBreakEvent e) {
+        if (Game.isStarted() && Game.isPlayer(e.getPlayer())) {
+            Plot plot = null;
+            if (Game.getState() == GameState.BUILDING) {
+                plot = Game.getPlot(e.getPlayer());
+            } else if (Game.getState() == GameState.VOTING) {
+                plot = Game.getCurrentPlot();
+            }
+
+            if (plot == null)
+                return;
+
+            Location to = e.getBlock().getLocation();
+            Location loc = plot.getLocation();
+
+            if (Math.abs(to.getX() - loc.getX()) > 16 || Math.abs(to.getZ() - loc.getZ()) > 16
+                    || to.getY() < loc.getY() - 1 || to.getY() >= Plot.getMaxHeight()) {
+                e.setCancelled(true);
+                e.getPlayer().sendMessage("§cВы не можете выйти за пределы строительной площадки!");
+            }
+        }
+    }
+
+    @EventHandler
+    void onPlayerPlace(BlockPlaceEvent e) {
         if (Game.isStarted() && Game.isPlayer(e.getPlayer())) {
             Plot plot = null;
             if (Game.getState() == GameState.BUILDING) {
